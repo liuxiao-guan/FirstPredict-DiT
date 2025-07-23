3b63a3dd720d2516bf0a64335169b3fbec8be76f
warrentdrew <yipinzhu@outlook.com>
2025-07-23 11:17:59 +0800

    add BO2O2-4 experiment

--- TaylorSeer-DiT/scripts/com_ddp_2.sh
+++ TaylorSeer-DiT/scripts/com_ddp_2.sh
@@ -7,51 +7,52 @@ CUDA_VISIBLE_DEVICES=5 torchrun --nnodes=1 --nproc_per_node=1  --master_port=295
   --ddim-sample \
   --num-sampling-steps 50 \
   --interval 1 \
-  --max-order 1 \
-  --max_block_order 1 \
-  --threshold 0.014 \
-  --mid_cor 2 \
-
-
-
-CUDA_VISIBLE_DEVICES=5 torchrun --nnodes=1 --nproc_per_node=1  --master_port=29504 sample_ddp.py \
-  --model DiT-XL/2 \
-  --per-proc-batch-size 50 \
-  --image-size 256 \
-  --cfg-scale 1.5 \
-  --ddim-sample \
-  --num-sampling-steps 50 \
-  --interval 1 \
-  --max-order 1 \
-  --max_block_order 2 \
-  --threshold 0.014 \
-  --mid_cor 2 \
-
-CUDA_VISIBLE_DEVICES=5 torchrun --nnodes=1 --nproc_per_node=1  --master_port=29504 sample_ddp.py \
-  --model DiT-XL/2 \
-  --per-proc-batch-size 50 \
-  --image-size 256 \
-  --cfg-scale 1.5 \
-  --ddim-sample \
-  --num-sampling-steps 50 \
-  --interval 1 \
-  --max-order 1 \
-  --max_block_order 3 \
-  --threshold 0.014 \
-  --mid_cor 2 \
-
-CUDA_VISIBLE_DEVICES=5 torchrun --nnodes=1 --nproc_per_node=1  --master_port=29504 sample_ddp.py \
-  --model DiT-XL/2 \
-  --per-proc-batch-size 50 \
-  --image-size 256 \
-  --cfg-scale 1.5 \
-  --ddim-sample \
-  --num-sampling-steps 50 \
-  --interval 1 \
-  --max-order 1 \
+  --max-order 2 \
   --max_block_order 4 \
   --threshold 0.014 \
   --mid_cor 2 \
+
+
+
+CUDA_VISIBLE_DEVICES=5 torchrun --nnodes=1 --nproc_per_node=1  --master_port=29504 sample_ddp.py \
+  --model DiT-XL/2 \
+  --per-proc-batch-size 50 \
+  --image-size 256 \
+  --cfg-scale 1.5 \
+  --ddim-sample \
+  --num-sampling-steps 50 \
+  --interval 1 \
+  --max-order 3 \
+  --max_block_order 4 \
+  --threshold 0.014 \
+  --mid_cor 2 \
+
+CUDA_VISIBLE_DEVICES=5 torchrun --nnodes=1 --nproc_per_node=1  --master_port=29504 sample_ddp.py \
+  --model DiT-XL/2 \
+  --per-proc-batch-size 50 \
+  --image-size 256 \
+  --cfg-scale 1.5 \
+  --ddim-sample \
+  --num-sampling-steps 50 \
+  --interval 1 \
+  --max-order 4 \
+  --max_block_order 4 \
+  --threshold 0.014 \
+  --mid_cor 2 \
+
+# CUDA_VISIBLE_DEVICES=0 torchrun --nnodes=1 --nproc_per_node=1  --master_port=29504 sample_ddp.py \
+#   --model DiT-XL/2 \
+#   --per-proc-batch-size 50 \
+#   --image-size 256 \
+#   --cfg-scale 1.5 \
+#   --ddim-sample \
+#   --num-sampling-steps 50 \
+#   --interval 1 \
+#   --max-order 1 \
+#   --max_block_order 2 \
+#   --threshold 0.012 \
+#   --mid_cor 2 \
+
 
 
 # CUDA_VISIBLE_DEVICES=3 torchrun --nnodes=1 --nproc_per_node=1 --master_port=29501 sample_ddp.py \
